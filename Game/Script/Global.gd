b8f2a1d501492c9b5a2ce140f7251af67b5aef4b
BleedingHeartStudio <164254976+BleedingHeartStudio@users.noreply.github.com>
2024-03-30 12:47:27 -0700

    player go brrrrrrrrrrrrr
    its in the title player go brrrrrrrrr

--- Game/Script/Global.gd
+++ Game/Script/Global.gd
@@ -12,7 +12,8 @@ func _ready():
 	else:
 		Set_Settings()
 func Set_Settings():
-	if ProjectSettings.has_setting("global/text_Resize"): ProjectSettings.set_setting("global/text_Resize", Config.get_value("global", "text_resize"))
+	if ProjectSettings.has_setting("global/text_Resize"): 
+		ProjectSettings.set_setting("global/text_Resize", Config.get_value("global", "text_resize"))
 	else: print_debug("SETTING_ERR_1")
 func Apply_Settings(TextResize = null):
 	if TextResize != null: Config.set_value("global", "text_resize", TextResize)
--- Game/testplayer.gd
+++ Game/testplayer.gd
@@ -1,9 +1,17 @@
 extends CharacterBody3D
 
-var speed = 10
+var speed = 1000
 var gravity  = 5
+var settiing = 0.1
+var sensitivity = 0.01
 @onready var camera = $Camera3D
 
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotation.y += -event.relative.x*sensitivity
+		rotation.x = clamp(rotation.x+event.relative.y*sensitivity, deg_to_rad(-70), deg_to_rad(70))
 func _physics_process(delta):
 	if Input.is_action_pressed("Forward"):
 		velocity.z = speed
@@ -18,4 +26,5 @@ func _physics_process(delta):
 		velocity.x = -speed
 	else:
 		velocity.x = 0
+	velocity = ((transform.basis * velocity.normalized()*speed)*delta)*Vector3(1,0,1)
 	move_and_slide()
