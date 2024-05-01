fb5e5f596a35df17ba9d0a1171ca912e94900795
BleedingHeartStudio <164254976+BleedingHeartStudio@users.noreply.github.com>
2024-05-01 10:31:44 -0700

    guess what does'nt work

--- Game/StateMachine'/States/Run.gd
+++ Game/StateMachine'/States/Run.gd
@@ -1,2 +1,13 @@
 extends Base_State
 class_name Run
+var speed = 10
+
+func _physics_process(delta):
+	if Input.is_action_pressed("Forward"):
+		State_Machine.velocity.z = speed
+	if Input.is_action_pressed("Backwards"):
+		State_Machine.velocity.z = -speed
+	if Input.is_action_pressed("Left"):
+		State_Machine.velocity.x = speed
+	if Input.is_action_pressed("Right"):
+		State_Machine.velocity.x = -speed
